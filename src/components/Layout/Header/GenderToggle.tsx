e8c49e3cc583bcecc5872fefc37878a9eed7e8c0
Uday vara <varaudayd@gmail.com>
2025-05-15 23:55:48 +0530

    fixed mobile gender buttons toggle issue

--- src/components/Layout/Header/GenderToggle.tsx
+++ src/components/Layout/Header/GenderToggle.tsx
@@ -1,10 +1,10 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./GenderToggle.css"
 import { getCookie } from '@/lib/cookie';
 import { usePathname } from 'next/navigation';
 function GenderToggle() {
-    const defaultGender = getCookie("gender") || "male"
+  const [gender,setGender] = useState(getCookie("gender") || "male")
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => { 
         if(!e.target.checked){
             document.cookie = "gender=male";
@@ -15,11 +15,15 @@ function GenderToggle() {
         }
      };
      const path = usePathname()
+     
+     useEffect(() => {
+       setGender(getCookie("gender") || "male")
+     },[window.document.cookie])
   return (
     <>
     {(path == "/" || path == "/products") && <div className='hidden flex-row items-center gap-3 lg:flex'>
 <h3 className='flex font-semibold gap-1 text-[#2e80f2]'>Him <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#2e80f2" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-mars-icon lucide-mars"><path d="M16 3h5v5"/><path d="m21 3-6.75 6.75"/><circle cx="10" cy="14" r="6"/></svg></h3>
-  <input className="switch" type="checkbox" defaultChecked={(defaultGender === "male" ? false : true)} onChange={handleChange} />
+  <input checked={(gender === "male" ? false : true)} className="switch" type="checkbox"  onChange={handleChange} />
   <h3 className='flex gap-1 font-semibold text-[#ec4899]'>Her <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ec4899" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-venus-icon lucide-venus"><path d="M12 15v7"/><path d="M9 19h6"/><circle cx="12" cy="9" r="6"/></svg></h3>
     </div>}
     </>
--- src/components/Layout/Header/MobileGenderButtons.tsx
+++ src/components/Layout/Header/MobileGenderButtons.tsx
@@ -2,10 +2,10 @@
 
 import { getCookie } from '@/lib/cookie';
 import { usePathname } from 'next/navigation';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function MobileGenderButtons() {
-     const defaultGender = getCookie("gender") || "male"
+    const [gender,setGender] = useState(getCookie("gender") || "male")
         const handleChange = (gender : "male" | "female") => { 
             if(gender == "male"){
                 document.cookie = "gender=male";
@@ -16,11 +16,14 @@ function MobileGenderButtons() {
             }
          };
          const path = usePathname()
+         useEffect(() => {
+                setGender(getCookie("gender") || "male")
+              },[window.document.cookie])
   return (
     <>
        {(path == "/" || path == "/products") && <div className="lg:hidden grid border-b-gray-700 border-b grid-cols-12  w-full ">
-            <div className={`col-span-6 text-center text-lg py-1 transition-all duration-300 border-t border-t-gray-00 border-r-primary border-r ${defaultGender == "male" ? "bg-primary text-white":"bg-white text-primary"}`} onClick={()=>{handleChange("male")}}>Men</div>
-            <div className={`col-span-6 text-center text-lg py-1 transition-all duration-300 border-t border-t-gray-00 ${defaultGender == "female" ? "bg-primary text-white":"bg-white text-primary"}`} onClick={()=>{handleChange("female")}}>Women</div>
+            <div className={`col-span-6 text-center text-lg py-1 transition-all duration-300 border-t border-t-gray-00 border-r-primary border-r ${gender == "male" ? "bg-primary text-white":"bg-white text-primary"}`} onClick={()=>{handleChange("male")}}>Men</div>
+            <div className={`col-span-6 text-center text-lg py-1 transition-all duration-300 border-t border-t-gray-00 ${gender == "female" ? "bg-primary text-white":"bg-white text-primary"}`} onClick={()=>{handleChange("female")}}>Women</div>
         </div>}
     </>
   )
