a9afaed58b49d723af4890016f0a738fe9a8aacd
Uday vara <varaudayd@gmail.com>
2025-06-08 23:55:46 +0530

    updated threshhold

--- src/app/(Pages)/products/_components/ProductsContainer.tsx
+++ src/app/(Pages)/products/_components/ProductsContainer.tsx
@@ -139,7 +139,7 @@ function ProductsContainer({
           
             <InfiniteScroll
               pageStart={0}
-              threshold={0}
+              threshold={150}
               className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:gap-8 gap-3 w-full pb-20"
               loadMore={() =>setTimeout(() => fetchNextPageData(),200)}
               hasMore={total > response?.data?.data?.length || false}
